d8724af43ea6c1c82d5dd8b21b0c5f9d1d75c066
sankar00 <70199313+sankar00@users.noreply.github.com>
2023-08-19 16:26:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ async def predict_sales(start_date: str = Form(...), end_date: str = Form(...)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
